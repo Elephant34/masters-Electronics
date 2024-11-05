d364f815c09222a13bec0df75eff432b5506966c
Elephant34 <51174423+Elephant34@users.noreply.github.com>
2024-11-05 11:26:07 +0000

    Added comments and directory of trial setups

--- MBiol.py
+++ MBiol.py
@@ -5,7 +5,7 @@ Description: The code controlling the mainloop of the electronics in my masters 
 """
 
 import tkinter as tk
-from random import randint
+from random import choices
 
 class TunnelElectronics:
     """Main class with control over the tunnel electronics
@@ -33,6 +33,8 @@ class TunnelDisplay(tk.Canvas):
         self.width = width
         self.height = height
 
+        # Makes the rectangles which will be configured to white or black depending on the trial
+        # Default colours are just for testing
         self.left_rect = self.create_rectangle(0, 0, self.width/2, self.height, fill="#ff00ff", outline="#ff00ff")
         self.right_rect = self.create_rectangle(self.width/2, 0, self.width, self.height, fill="#00ffff", outline="#00ffff")
 
@@ -43,14 +45,61 @@ class TunnelDisplay(tk.Canvas):
 def exit_mainloop():
     """Safley closes the program
     """
-    # Gets the running variable from the 
+    # Gets the running variable from global scope
     global running
 
+    # Sets to false to cause the program to exit
     running = False
 
     return
 
 DEBUG = False
+
+# List of the experimental trial setups
+EXPERIMENTAL_TRIALS = {
+    # Experiment Set 1 (see method notes)
+    1: {
+        "left_rect": "bright",
+        "right_rect": "dark",
+        "obstacle": "WW"
+    },
+    2: {
+        "left_rect": "dark",
+        "right_rect": "bright",
+        "obstacle": "WW"
+    },
+    3: {
+        "left_rect": "bright",
+        "right_rect": "dark",
+        "obstacle": "PP"
+    },
+    4: {
+        "left_rect": "dark",
+        "right_rect": "bright",
+        "obstacle": "PP"
+    },
+    # Experiment Set 2 (see method notes)
+    5: {
+        "left_rect": "bright",
+        "right_rect": "bright",
+        "obstacle": "WP"
+    },
+    6: {
+        "left_rect": "bright",
+        "right_rect": "bright",
+        "obstacle": "PW"
+    },
+    7: {
+        "left_rect": "dark",
+        "right_rect": "dark",
+        "obstacle": "WP"
+    },
+    8: {
+        "left_rect": "dark",
+        "right_rect": "dark",
+        "obstacle": "PW"
+    },
+}
 
 # Mainloop of the electronics
 if __name__ == "__main__":
